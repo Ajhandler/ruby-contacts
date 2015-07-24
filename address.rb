96c73850a1e134664521bc689f882693a8a3b6db
Alexander Handler <Alexjhandler@gmail.com>
2015-07-23 21:19:20 -0400

    add address class

---
+++ address.rb
@@ -0,0 +1,16 @@
+class Address
+  attr_accessor :kind, :street_1, :street_2, :city, :state, :postal_code
+
+  def to_s(format = 'short')
+    address = ''
+    case format
+    when 'short'
+      address += "#{kind}: "
+      address += "#{street_1}"
+      if street_2
+        address += " " + street_2
+      end
+      address += ", #{city}, #{state}, #{postal_code}"
+    end
+  end
+end
