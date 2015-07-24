45ebf7149d7164de746325ac3f09828a5f101235
Alexander Handler <Alexjhandler@gmail.com>
2015-07-23 23:13:30 -0400

    add simple search on contacts

--- address_book.rb
+++ address_book.rb
@@ -6,6 +6,24 @@ class AddressBook
     @contacts = []
   end
 
+  def find_by_name(name)
+    results = []
+    search = name.downcase
+    contacts.each do |contact|
+      if contact.first_name.downcase.include?(search) ||
+        contact.last_name.downcase.include?(search)
+        results.push(contact)
+      end
+    end
+    puts "name search result (#{search})"
+    results.each do |contact|
+      puts contact.to_s('full_name')
+      puts contact.print_phone_numbers
+      puts contact.print_addresses
+      puts "\n"
+    end
+  end
+
   def print_contact_list
     puts "Contact List"
     contacts.each do |contact|
@@ -15,16 +33,12 @@ class AddressBook
 end
 
 address_book = AddressBook.new
-
-alex = Contact.new
-alex.first_name = "Alex"
-alex.last_name = "Handler"
-alex.add_phone_number("home", "203-849-3329")
-alex.add_phone_number("home", "203-234-3329")
-alex.add_address("home","22 Assisi Way", "Apt c","Norwalk","ct","06851")
-address_book.contacts.push(alex)
-jill = Contact.new
-jill.first_name = "Jill"
-jill.last_name = "Daley"
-address_book.contacts.push(jill)
-puts address_book.print_contact_list
+david = Contact.new
+david.first_name = "David"
+david.middle_name = "Gregory"
+david.last_name = "Peters"
+david.add_phone_number("home", "203-849-3329")
+david.add_address("home","22 Assisi Way", "","Norwalk","CT", "06851")
+address_book.contacts.push(david)
+address_book.find_by_name("s")
+puts david.inspect
